c52185897b31aa32bad8c658b58d91da5874caaf
Samuel Salcedo <samuelsalcedoramirez@gmail.com>
2024-09-02 14:53:01 -0500

    terminado

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -41,7 +41,7 @@
                                                     @foreach($post->images as $image)
 
 
-                                                        <img src="{{ asset('storage/uploads' . $image->image_url) }}" width="100" class="rounded mb-2" alt="Imagen del post {{ $post->id }}">
+                                                        <img src="{{ asset('storage/' . $image->image_url) }}" width="100" class="rounded mb-2" alt="Imagen del post {{ $post->id }}">
                                                     @endforeach
                                                 @else
                                                     No image
