20443a0ff37c8b0fa2ccfd4000588524a62054dc
Samuel Salcedo <samuelsalcedoramirez@gmail.com>
2024-09-06 20:07:28 -0500

    terminado

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -40,7 +40,7 @@
                                                 <td class="py-4 px-6">
                                                     @if($post->images->isNotEmpty())
                                                         @foreach($post->images as $image)
-                                                            <img src="{{ asset('storage/' . $image->image_url) }}" width="100" class="rounded mb-2" alt="Imagen del post {{ $post->id }}">
+                                                            <img src="{{ asset('storage/app/public' . $image->image_url) }}" width="100" class="rounded mb-2" alt="Imagen del post {{ $post->id }}">
                                                         @endforeach
                                                     @else
                                                         No image
