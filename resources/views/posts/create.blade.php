3ebf332ec4dccb50240a9ab4a6bbb853a2411d76
Samuel Salcedo <samuelsalcedoramirez@gmail.com>
2024-09-07 15:34:49 -0500

    terminado

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -23,13 +23,13 @@
                     </div>
                     <div>
                         <label for="title" class="block mb-2 text-sm font-medium text-red-700">Título</label>
-                        <input type="text" id="title" name="title" value="{{ isset($post->title) }}" class="bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Título del Post" required />
+                        <input type="text" id="title" name="title" class="bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Título del Post" value="{{ old('title') }}" required />
                     </div>
                 </div>
 
                 <div class="mb-6">
                     <label for="body" class="block mb-2 text-sm font-medium text-red-700">Contenido</label>
-                    <textarea id="body" name="body" class="bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Contenido del Post" required>{{ isset($post->body) }}</textarea>
+                    <textarea id="body" name="body" class="bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Contenido del Post" required>{{ old('body') }}</textarea>
                 </div>
 
                 <div class="mb-6">
@@ -41,26 +41,19 @@
                     </select>
                 </div>
 
-                {{-- puse eso --}}
                 <div class="mb-6">
-                    <label for="image_url" class="block mb-2 text-sm font-medium text-red-700">Imagen</label>
-                    @if (isset($post->image_url))
-                        <img src="{{ asset('storage/'.$post->image_url) }}" width="100" alt="Imagen del Post" class="mb-4 rounded-lg shadow-sm">
-                    @endif
-                    <input type="file" id="image_url" name="image_url" class="bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5">
+                    <label for="image_url" class="block mb-2 text-sm font-medium text-red-700">Imagen Principal</label>
+                    <input type="file" id="image_url" name="image_url" class="bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" />
                 </div>
-                {{-- <div class="mb-6">
-                    <label for="images" class="block mb-2 text-sm font-medium text-red-700">Imágenes</label>
-                    <input type="file" name="images[]" id="images" multiple class="bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5">
-                </div> --}}
 
+                <div class="mb-6">
+                    <label for="images" class="block mb-2 text-sm font-medium text-red-700">Imágenes Adicionales</label>
+                    <input type="file" id="images" name="images[]" class="bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" multiple />
                 </div>
-                <button type="submit" class="w-full sm:w-auto px-5 py-2.5 text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm text-center">Guardar Datos</button>
+
+                <button type="submit" class="bg-red-600 text-white font-bold py-2 px-4 rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500">Guardar</button>
             </form>
-
-            <a href="/posts" class="text-red-600 hover:underline mt-4 inline-block text-center w-full">Regresar</a>
         </div>
     </div>
-
 </body>
 </html>
